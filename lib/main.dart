6eb8a3f95f2e272447eb6c39652d5d4515313ab9
HaWkEYE VIDAA <sidddverrr@gmail.com>
2021-08-18 17:03:49 +0530

    YCT

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:untitled/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Yellow Class Task',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
